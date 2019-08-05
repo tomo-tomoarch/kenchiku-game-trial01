50773cf1910fea6eac7de9a8682837fa24003ab7
tomo-tomoarch <43036125+tomo-tomoarch@users.noreply.github.com>
2019-08-05 20:05:35 +0900

    brief 3.00.0.0
    number test

---
+++ Kenchiku-game-trial-01/Assets/Instantiatetest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiatetest : MonoBehaviour
+{
+    public GameObject original;
+    public int j;
+        
+        // Start is called before the first frame update
+    void Start()
+    {
+        int i;
+        int k;
+        int l;
+
+        for (i = 0; i < j; i++)
+        {
+            k = Random.Range(1, j);
+            l = Random.Range(1, j);
+
+            Vector3 position = new Vector3(l, k, i);
+            Instantiate(original, position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        
+}
